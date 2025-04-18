616e8e991a5bf2b2b7c9db212a670aa3d24bed3f
Artem016 <lenovaega2@gmail.com>
2025-04-18 14:54:18 +0500

    Добавьте файлы проекта.

---
+++ RentalApp/DbContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace RentalApp
+{
+    internal class DbContext
+    {
+        private readonly string _connectionString = "Server=HOME-PC\\SQLEXPRESS;Database=RentalDB;TrustServerCertificate=True;Trusted_Connection=True;";
+
+        public DataTable GetClints()
+        {
+            DataTable table = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("select * from Clients", connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(table);
+            }
+
+            return table;
+        }
+    }
+}
